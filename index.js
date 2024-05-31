faaf17eb959581c0595c13004e6c1bdbcdaaac8d
JhunnirieFer <jcbuensalida06@gmail.com>
2024-05-31 11:32:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ function hitLikes1() {
   countLikes1.textContent = totalLikes1.toString()
 }
 btnLike1.addEventListener("click",hitLikes1)
+function hitLikes2() {
+  let totalLikes2 = parseInt(countLikes2.value) + 1
+  countLikes1.textContent = totalLikes2.toString()
+}
+btnLike2.addEventListener("click",hitLikes1)
+function mistakes2() {
+  let totalDisLikes1 = parsenInt(countDisLikes2.value) + 1
+  countDisLikes1. textContent = toalDisLikes1. toString() 
+}
+btnDisLike1.addEventListener("click",misLikes1) 
+
+function mistakes2() {
+  let totalDisLikes2 = parsenInt(countedDisLikes2.value) + 1
+  countDisLikes2. textContent = toalDisLikes2. toString() 
+}
+btnDisLike1.addEventListener("click",misLikes2) 
